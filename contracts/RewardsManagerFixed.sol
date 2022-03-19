a2f39cc88fce3673f80bdf80db2ce1047beb5210
Fei Yang <fyang1024@gmail.com>
2022-03-19 22:38:40 +1100

    remove redundant blank lines

--- contracts/RewardsManagerFixed.sol
+++ contracts/RewardsManagerFixed.sol
@@ -57,9 +57,6 @@ contract RewardsManagerFixed is ReentrancyGuard {
     // User share of token X is equal to tokensForEpoch * points[epochId][vaultId][userAddress] / totalPoints[epochId][vaultAddress]
     // You accrue one point per second for each second you are in the vault
 
-
-
-
     mapping(uint256 => mapping(address => mapping(address => uint256))) public rewards; // rewards[epochId][vaultAddress][tokenAddress] = AMOUNT
 
     /// @dev Sets the new epoch
