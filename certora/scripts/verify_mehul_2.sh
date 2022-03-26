39e6448856445b9af3cf9f6f86ed7162b3ae6b61
modernAlcibiades <mehul.kumar@columbia.edu>
2022-03-26 18:14:08 +0900

    Certora tool is broken. Imports old spec that no longer exists

---
+++ certora/scripts/verify_mehul_2.sh
@@ -0,0 +1,11 @@
+solc-select use 0.8.10
+
+certoraRun certora/harnesses/RewardsManagerHarness.sol certora/helpers/DummyERC20A.sol certora/helpers/DummyERC20B.sol \
+    --verify RewardsManagerHarness:certora/specs/Badger_mehul_2.spec \
+    --send_only \
+    --optimistic_loop \
+    --loop_iter 1 \
+    --packages @oz=certora/openzeppelin/contracts \
+    --msg "$1" $2
+
+#     --loop_iter 2   --rule $1 \
