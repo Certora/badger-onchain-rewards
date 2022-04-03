b56b1f6238488fc9172ed04ab94a4b00ed7987ad
Aleksander Kryukov <58052996+RedLikeRosesss@users.noreply.github.com>
2022-04-03 01:04:21 +0100

    delete basic script

--- certora/scripts/verifyRMAlex.sh
+++
@@ -1,9 +0,0 @@
-certoraRun certora/harnesses/RewardsManagerHarness.sol certora/helpers/DummyERC20A.sol certora/helpers/DummyERC20B.sol \
-    --verify RewardsManagerHarness:certora/specs/itsLikeAReward.spec \
-    --solc solc8.10 \
-    --optimistic_loop \
-    --cloud \
-    --loop_iter 1 \
-    --packages @oz=certora/openzeppelin/contracts \
-    # --rule "$1" \
-    # --msg "$1"
