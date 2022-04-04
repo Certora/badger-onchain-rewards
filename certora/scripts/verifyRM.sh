cb692f3acd8d52dc0a1e9c1041720024124e92f0
Ariel Grosh <ariel241202@gmail.com>
2022-04-04 12:13:17 +0300

    updated script

--- certora/scripts/verifyRM.sh
+++ certora/scripts/verifyRM.sh
@@ -3,6 +3,8 @@ solc-select use 0.8.10
 certoraRun certora/harnesses/RewardsManagerHarness.sol certora/helpers/DummyERC20A.sol certora/helpers/DummyERC20B.sol \
     --verify RewardsManagerHarness:certora/specs/itsLikeAReward.spec \
     --send_only \
-    --loop_iter 2 \
---staging    --packages @oz=certora/openzeppelin/contracts \
+    --optimistic_loop \
+    --loop_iter 1 \
+    --staging \
+    --packages @oz=certora/openzeppelin/contracts \
     --msg "$1"
